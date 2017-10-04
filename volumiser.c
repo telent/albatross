1a63e5b77097f0deafde62f36515f67dd755fee8
Daniel Barlow <dan@telent.net>
2017-10-04 16:28:30 +0100

    graft tcp server onto volumiser

--- volumiser.c
+++ volumiser.c
@@ -1,8 +1,65 @@
+/*
+ * tcpserver.c - A simple TCP echo server
+ * usage: tcpserver <port>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
 
-int main (int argc, char *argv[])
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFSIZE 1024
+
+#if 0
+/*
+ * Structs exported from in.h
+ */
+
+/* Internet address */
+struct in_addr {
+  unsigned int s_addr;
+};
+
+/* Internet style socket address */
+struct sockaddr_in  {
+  unsigned short int sin_family; /* Address family */
+  unsigned short int sin_port;   /* Port number */
+  struct in_addr sin_addr;	 /* IP address */
+  unsigned char sin_zero[...];   /* Pad to size of 'struct sockaddr' */
+};
+
+/*
+ * Struct exported from netdb.h
+ */
+
+/* Domain name service (DNS) host entry */
+struct hostent {
+  char    *h_name;        /* official name of host */
+  char    **h_aliases;    /* alias list */
+  int     h_addrtype;     /* host address type */
+  int     h_length;       /* length of address */
+  char    **h_addr_list;  /* list of addresses */
+}
+#endif
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int send_volume (char * device, int fd)
 {
   int i, j;
   int err;
@@ -15,15 +72,18 @@ int main (int argc, char *argv[])
   /*  int bufsize = 2 * (16/8) * 44100 * 1;  */
   int bufsize = 128;
 
+  int bytes;
+  char out_buf[40];
+
   if((buf = calloc(bufsize, 2 * (sizeof (short)))) == 0) {
     fprintf (stderr, "cannot allocate buffer (size %d)",
              bufsize);
     exit (1);
   }
 
-  if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+  if ((err = snd_pcm_open (&capture_handle, device, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
     fprintf (stderr, "cannot open audio device %s (%s)\n",
-             argv[1],
+             device,
              snd_strerror (err));
     exit (1);
   }
@@ -96,7 +156,9 @@ int main (int argc, char *argv[])
 #define SMOOTH (2<<4)
       amp = (SMOOTH-1)*(amp/SMOOTH) + (max-min)/SMOOTH;
       if((i % 100) == 0) {
-        fprintf(stderr, " hey %*s\n", (max-min), amp/500, "*");
+        fprintf(stderr, " hey %*s\n",  amp/500, "*");
+        bytes = sprintf(out_buf, "%d\n", amp);
+        write(fd, out_buf, bytes);
         i=1;
       } else {
         i++;
@@ -108,4 +170,115 @@ int main (int argc, char *argv[])
   free(buf);
   snd_pcm_close (capture_handle);
   exit (0);
+}
+
+int main(int argc, char **argv) {
+  int parentfd; /* parent socket */
+  int childfd; /* child socket */
+  int portno; /* port to listen on */
+  int clientlen; /* byte size of client's address */
+  struct sockaddr_in serveraddr; /* server's addr */
+  struct sockaddr_in clientaddr; /* client addr */
+  struct hostent *hostp; /* client host info */
+  char buf[BUFSIZE]; /* message buffer */
+  char *hostaddrp; /* dotted decimal host addr string */
+  int optval; /* flag value for setsockopt */
+  int n; /* message byte size */
+
+  /*
+   * check command line arguments
+   */
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s <port> <device>\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[1]);
+
+  /*
+   * socket: create the parent socket
+   */
+  parentfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (parentfd < 0)
+    error("ERROR opening socket");
+
+  /* setsockopt: Handy debugging trick that lets
+   * us rerun the server immediately after we kill it;
+   * otherwise we have to wait about 20 secs.
+   * Eliminates "ERROR on binding: Address already in use" error.
+   */
+  optval = 1;
+  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
+	     (const void *)&optval , sizeof(int));
+
+  /*
+   * build the server's Internet address
+   */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+
+  /* this is an Internet address */
+  serveraddr.sin_family = AF_INET;
+
+  /* let the system figure out our IP address */
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* this is the port we will listen on */
+  serveraddr.sin_port = htons((unsigned short)portno);
+
+  /*
+   * bind: associate the parent socket with a port
+   */
+  if (bind(parentfd, (struct sockaddr *) &serveraddr,
+	   sizeof(serveraddr)) < 0)
+    error("ERROR on binding");
+
+  /*
+   * listen: make this socket ready to accept connection requests
+   */
+  if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */
+    error("ERROR on listen");
+
+  /*
+   * main loop: wait for a connection request, echo input line,
+   * then close connection.
+   */
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+    /*
+     * accept: wait for a connection request
+     */
+    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (childfd < 0)
+      error("ERROR on accept");
+
+    /*
+     * gethostbyaddr: determine who sent the message
+     */
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      error("ERROR on gethostbyaddr");
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      error("ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n",
+	   hostp->h_name, hostaddrp);
+
+    /*
+     * read: read input string from the client
+     */
+#if 0
+    bzero(buf, BUFSIZE);
+    n = read(childfd, buf, BUFSIZE);
+    if (n < 0)
+      error("ERROR reading from socket");
+    printf("server received %d bytes: %s", n, buf);
+#endif
+    /*
+     * write: echo the input string back to the client
+     */
+
+    send_volume(argv[2], childfd);
+    close(childfd);
+  }
 }
